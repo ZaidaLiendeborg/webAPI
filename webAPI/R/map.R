e08751f7dc380cf2ff3265c92c938790ceaba578
hejemma <emmwa513@student.liu.se>
2016-09-28 14:30:53 +0200

    rox map

--- webAPI/R/map.R
+++ webAPI/R/map.R
@@ -1,12 +1,21 @@
-
+#' Plotting a map from coordinates
+#' 
+#' The function plots a map of selectable types and the wanted zoom of the map. 
+#' @param dat A object returned from the function  \code{\link{get_coor}}.
+#' @param zoom A integer between 3 and 20 depending on how zoomed in you want the map, high values for big zoom.
+#' @param typemap A character string of which kind of map you want. Possible choices are "roadmap","satellite","hybrid" and "terrain". 
+#' @return A map over the area from the coordinates in the object from \code{\link{get_coor}}
+#' @examples 
+#' target<-getURL2R("Linkoping, Sweden")
+#' CordObj<-get_coor(target)
+#' plot_map(CordObj, zoom=10, typemap="roadmap")
+#' @export
 plot_map<-function(dat, zoom, typemap){
   latitude <- dat$lat
   longitude <- dat$long
-  place <- dat$results[[1]]$formatted_address
-  lat <- c(latitude-1, latitude +1) #define our map's ylim
-  lon <- c(longitude -1, longitude +1) #define our map's xlim
+  lat <- c(latitude-1, latitude +1) 
+  lon <- c(longitude -1, longitude +1) 
 
-# getting the map
   themap <- get_map(location = c(lon = mean(lon), lat = mean(lat)), zoom = zoom,
                       maptype = typemap, scale = 2 )
 
