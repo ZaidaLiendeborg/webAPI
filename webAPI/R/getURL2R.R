b8f8514ee7dda7c404d3de505437f127c26275bb
ZaidaLiendeborg <zaida@liendeborg.se>
2016-09-28 12:54:42 +0200

    byggt paket

--- webAPI/R/getURL2R.R
+++ webAPI/R/getURL2R.R
@@ -1,5 +1,3 @@
-
-
 #Geocoding
 #Getting URL
 
@@ -15,47 +13,4 @@ getURL2R<- function(path) {
   return(Robj)
 }
 
-target<- get_url("Linköping, Sweden")
 
-target
-
-
-#In use
-
-
-latitude <- dat$results[[1]]$geometry$location["lat"]
-longitude <- dat$results[[1]]$geometry$location["lng"]
-place <- dat$results[[1]]$formatted_address
-
-latitude
-longitude
-place
-
-dat$status
-
-#Getting a static map
-#Construct that URL in R using paste?
-base="http://maps.googleapis.com/maps/api/staticmap?center="
-latitude=55.75
-longitude=37.62
-zoom=13
-maptype="hybrid"
-suffix ="&size=800x800&sensor=false&format=png"
-
-
-#Possible solution
-base="http://maps.googleapis.com/maps/api/staticmap?center="
-latitude=55.75
-longitude=37.62
-zoom=13
-maptype="hybrid"
-suffix ="&size=800x800&format=png"
-
-target <- paste0(base,latitude,",",longitude,
-                 "&zoom=",zoom,"&maptype=",maptype,suffix)
-
-
-#What to do next...?
-download.file(target,"test.png", mode = "wb")
-
-class(dat)
--- webAPI/R/get_coor.R
+++ webAPI/R/get_coor.R
@@ -1,15 +1,11 @@
 #JSON to R object
 
 get_coor<- function(Robj){
-  
   lat<- Robj$results[[1]]$geometry$location["lat"]
   long<- Robj$results[[1]]$geometry$location["lng"]
   place<- Robj$results[[1]]$formatted_address
-    
   coor<- list(lat=lat, long=long, place=place)
   return(coor)
 }
-
-get_coor(target)
 
 
