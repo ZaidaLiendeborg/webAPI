36e4fa0f035cecfb0705825182bf658eb6855b8c
ZaidaLiendeborg <zaida@liendeborg.se>
2016-09-28 15:05:49 +0200

    Added more unit test. Stop argument in get_coor. Import in package dependencies.

--- webAPI/R/get_coor.R
+++ webAPI/R/get_coor.R
@@ -1,9 +1,15 @@
 #JSON to R object
 
 get_coor<- function(Robj){
-  lat<- Robj$results[[1]]$geometry$location["lat"]
-  long<- Robj$results[[1]]$geometry$location["lng"]
-  place<- Robj$results[[1]]$formatted_address
-  coor<- list(lat=lat, long=long, place=place)
-  return(coor)
+  if (class(Robj) != "list"){
+    stop("Robj must be a list.")
+  } 
+  else{
+    lat<- Robj$results[[1]]$geometry$location["lat"]
+    long<- Robj$results[[1]]$geometry$location["lng"]
+    place<- Robj$results[[1]]$formatted_address
+    coor<- list(lat=lat, long=long, place=place)
+    return(coor)
+  }
+  
 }
--- webAPI/tests/testthat/test.R
+++ webAPI/tests/testthat/test.R
@@ -1,25 +1,25 @@
 test_that("Testing getURL2R()",{
   test1<- getURL2R("Linköping,Sweden")
-  test2<- getURL2R(12345)
   
   expect_that(getURL2R, is_a("function"),
               info = "Error: getURL2R is not a function.")
   expect_that(all(names(formals(getURL2R)) %in% c("path")), 
               condition=is_true(), info = "Error: API argument name is wrong.")
   expect_that(test1, is_a("list"), info= "Error: API did not return an R list.")
-  expect_error(test2, info="Error: Path should be a character string.")
+  expect_error(getURL2R(123), info="Error: Insert character string.")
   expect_equal(test1$status, "OK", info= "Error: API request failed")
 }
 )
 
 test_that("Testing get_coor()",{
-  test1<- getURL2R("Linköping,Sweden")
+  test1<- getURL2R("Linkoping,Sweden")
   coor1<- get_coor(test1)
   
-  expect_that(getURL2R, is_a("function"),
+  expect_that(get_coor, is_a("function"),
               info = "Error: getURL2R is not a function.")
-  expect_that(all(names(formals(getURL2R)) %in% c("path")), 
+  expect_that(all(names(formals(get_coor)) %in% c("Robj")), 
               condition=is_true(), info = "Error: API argument name is wrong.")
+  expect_that(test1, is_a("list"), info= "Error: API did not return an R list.")
   
 }
 )
